847e5b83395c6dc4618c6759ccf600272f9ada8a
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-09-05 16:07:07 +0200

    Added variables to Person and had the csutomer class inherit from Person

--- GuestBookingSystem/Business/Customer.cs
+++ GuestBookingSystem/Business/Customer.cs
@@ -6,38 +6,24 @@ using System.Threading.Tasks;
 
 namespace GuestBookingSystem.Business
 {
-    internal class Customer
+    internal class Customer : Person
     {
         #region Instance Variables
-        private String name;
-        private String surname;
+        
         private int custID;
-        private String phone;
-        private String email;
-        private String country;
         private String cardNumber;
 
         #endregion
 
         #region Constructor
-        public Customer() {
+        public Customer(String cardTemp) : base() {
+            cardNumber = cardTemp;
         }
 
         #endregion
 
         #region Property Members
-        public string Name
-        {
-            get { return name; } 
-            set { name = value; }
-
-        }
-
-        public String Surname
-        {
-            get { return surname; }
-            set { surname = value; }
-        }
+       
 
         public int CustID
         {
@@ -45,23 +31,6 @@ namespace GuestBookingSystem.Business
             set { custID = value; }
         }
 
-        public String Phone
-        {
-            get { return phone; }
-            set { phone = value; }
-        }
-
-        public String Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
-
-        public String Country
-        {
-            get { return country; }
-            set { country = value; }
-        }
 
         public String CardNumber
         {
