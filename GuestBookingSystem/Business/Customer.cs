15e64fa6d8ef41458fecfbc160e0b4c738ec342f
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-09-05 15:53:19 +0200

    Added instance variables, constructor, and getters and setters

--- GuestBookingSystem/Business/Customer.cs
+++ GuestBookingSystem/Business/Customer.cs
@@ -8,8 +8,66 @@ namespace GuestBookingSystem.Business
 {
     internal class Customer
     {
-
+        #region Instance Variables
         private String name;
-        private int id;
+        private String surname;
+        private int custID;
+        private String phone;
+        private String email;
+        private String country;
+        private String cardNumber;
+
+        #endregion
+
+        #region Constructor
+        public Customer() {
+        }
+
+        #endregion
+
+        #region Property Members
+        public string Name
+        {
+            get { return name; } 
+            set { name = value; }
+
+        }
+
+        public String Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public int CustID
+        {
+            get { return custID; }
+            set { custID = value; }
+        }
+
+        public String Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public String Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public String Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public String CardNumber
+        {
+            get { return cardNumber; }
+            set { cardNumber = value; }
+        }
+
+        #endregion 
     }
-}
