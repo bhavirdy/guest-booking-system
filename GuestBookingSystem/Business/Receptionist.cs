4d06cfb7d192b31038b0e20036d4408717df5e6c
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-09-05 17:03:16 +0200

    Added variables, constructor, and property methods to receptionist

--- GuestBookingSystem/Business/Receptionist.cs
+++ GuestBookingSystem/Business/Receptionist.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace GuestBookingSystem.Business
 {
-    internal class Receptionist
+    internal class Receptionist : Person
     {
+
+        #region Data Members
+        private int recpNum;
+        private int salary;
+
+        #endregion
+
+        #region Property Members
+
+        public int ReceptionistNum
+        {
+            get { return recpNum; }
+            set { recpNum = value; }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+        public Receptionist(int rNum, int salTemp) : base()
+        {
+            this.recpNum = rNum;
+            this.salary = salTemp;
+        }
+
+        #endregion 
+
     }
 }
