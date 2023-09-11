0a047140c30f2c2c2b336e8f1f9465d8cae70d5c
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-09-11 21:37:31 +0200

    Added a constructor and methods to the BookingController Class

---
+++ GuestBookingSystem/Business/BookingController.cs
@@ -0,0 +1,49 @@
+﻿using GuestBookingSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBookingSystem.Business
+{
+    internal class BookingController
+    {
+        #region Data Members
+        BookingDB bookingDB;
+        Collection<Booking> bookingCollection;
+
+        #endregion
+
+        #region Property Methods
+        public Collection<Booking> BookingCollection
+        {
+            get { return BookingCollection; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public BookingController()
+        {
+            bookingDB = new bookingDB();
+            bookingCollection = bookingDB.AllBookings;
+
+        }
+
+        #endregion 
+
+        public void DataMaintanence(Booking bookTemp)
+        {
+            bookingDB.DataSetChange(bookTemp);
+            bookingCollection.Add(bookTemp);
+        }
+
+        public bool FinalizeChanges(Booking bookTemp)
+        {
+            return bookingDB.UpdateDataSource(bookTemp);
+        }
+    }
+}
