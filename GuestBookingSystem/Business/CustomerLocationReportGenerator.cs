1da0f78ecd1cda10bfcd38623338e6308a938816
Bhavishyan Reddy <bhavirdy@gmail.com>
2023-09-17 22:24:58 +0200

    added customerlocationreport class

---
+++ GuestBookingSystem/Business/CustomerLocationReportGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBookingSystem.Business
+{
+    internal class CustomerLocationReportGenerator
+    {
+        public CustomerLocationReportGenerator() { }
+
+        public void generateCustomerLocationReport()
+        {
+
+        }
+    }
+}
