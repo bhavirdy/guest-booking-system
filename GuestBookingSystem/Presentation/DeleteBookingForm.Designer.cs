a41f022043e3e3a4b4771c4826f40fbd6bdf6166
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-09-19 15:09:47 +0200

    Added functionality to Delete a booking

--- GuestBookingSystem/Presentation/DeleteBookingForm.Designer.cs
+++ GuestBookingSystem/Presentation/DeleteBookingForm.Designer.cs
@@ -31,16 +31,9 @@
             this.btnClear = new System.Windows.Forms.Button();
             this.btnExitV = new System.Windows.Forms.Button();
             this.btnSubmitV = new System.Windows.Forms.Button();
-            this.txtRoomNum = new System.Windows.Forms.TextBox();
-            this.lblRoomNum = new System.Windows.Forms.Label();
-            this.txtCustID = new System.Windows.Forms.TextBox();
-            this.lblCustID = new System.Windows.Forms.Label();
-            this.dateTimePickerDepartureDate = new System.Windows.Forms.DateTimePicker();
-            this.lblLeaveDate = new System.Windows.Forms.Label();
-            this.lblADate = new System.Windows.Forms.Label();
-            this.dateTimePickerArrival = new System.Windows.Forms.DateTimePicker();
             this.lblHeadingEdit = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnClear
@@ -76,80 +69,6 @@
             this.btnSubmitV.Text = "Submit";
             this.btnSubmitV.UseVisualStyleBackColor = true;
             // 
-            // txtRoomNum
-            // 
-            this.txtRoomNum.Location = new System.Drawing.Point(204, 479);
-            this.txtRoomNum.Name = "txtRoomNum";
-            this.txtRoomNum.Size = new System.Drawing.Size(271, 22);
-            this.txtRoomNum.TabIndex = 35;
-            // 
-            // lblRoomNum
-            // 
-            this.lblRoomNum.AutoSize = true;
-            this.lblRoomNum.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRoomNum.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.lblRoomNum.Location = new System.Drawing.Point(33, 480);
-            this.lblRoomNum.Name = "lblRoomNum";
-            this.lblRoomNum.Size = new System.Drawing.Size(137, 21);
-            this.lblRoomNum.TabIndex = 34;
-            this.lblRoomNum.Text = "Room Number";
-            // 
-            // txtCustID
-            // 
-            this.txtCustID.Location = new System.Drawing.Point(204, 371);
-            this.txtCustID.Name = "txtCustID";
-            this.txtCustID.Size = new System.Drawing.Size(271, 22);
-            this.txtCustID.TabIndex = 33;
-            // 
-            // lblCustID
-            // 
-            this.lblCustID.AutoSize = true;
-            this.lblCustID.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCustID.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.lblCustID.Location = new System.Drawing.Point(33, 370);
-            this.lblCustID.Name = "lblCustID";
-            this.lblCustID.Size = new System.Drawing.Size(120, 21);
-            this.lblCustID.TabIndex = 32;
-            this.lblCustID.Text = "Customer ID";
-            // 
-            // dateTimePickerDepartureDate
-            // 
-            this.dateTimePickerDepartureDate.Location = new System.Drawing.Point(204, 443);
-            this.dateTimePickerDepartureDate.MinDate = new System.DateTime(2023, 9, 6, 0, 0, 0, 0);
-            this.dateTimePickerDepartureDate.Name = "dateTimePickerDepartureDate";
-            this.dateTimePickerDepartureDate.Size = new System.Drawing.Size(271, 22);
-            this.dateTimePickerDepartureDate.TabIndex = 31;
-            // 
-            // lblLeaveDate
-            // 
-            this.lblLeaveDate.AutoSize = true;
-            this.lblLeaveDate.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLeaveDate.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.lblLeaveDate.Location = new System.Drawing.Point(33, 443);
-            this.lblLeaveDate.Name = "lblLeaveDate";
-            this.lblLeaveDate.Size = new System.Drawing.Size(146, 21);
-            this.lblLeaveDate.TabIndex = 30;
-            this.lblLeaveDate.Text = "Departure Date";
-            // 
-            // lblADate
-            // 
-            this.lblADate.AutoSize = true;
-            this.lblADate.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblADate.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.lblADate.Location = new System.Drawing.Point(33, 406);
-            this.lblADate.Name = "lblADate";
-            this.lblADate.Size = new System.Drawing.Size(116, 21);
-            this.lblADate.TabIndex = 29;
-            this.lblADate.Text = "Arrival Date";
-            // 
-            // dateTimePickerArrival
-            // 
-            this.dateTimePickerArrival.Location = new System.Drawing.Point(204, 405);
-            this.dateTimePickerArrival.MinDate = new System.DateTime(2023, 9, 6, 0, 0, 0, 0);
-            this.dateTimePickerArrival.Name = "dateTimePickerArrival";
-            this.dateTimePickerArrival.Size = new System.Drawing.Size(271, 22);
-            this.dateTimePickerArrival.TabIndex = 28;
-            // 
             // lblHeadingEdit
             // 
             this.lblHeadingEdit.AutoSize = true;
@@ -162,35 +81,30 @@
             this.lblHeadingEdit.Text = "Delete Bookings";
             this.lblHeadingEdit.Click += new System.EventHandler(this.lblHeadingEdit_Click);
             // 
-            // listView1
+            // dataGridView1
             // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(37, 69);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(595, 260);
-            this.listView1.TabIndex = 26;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(733, 264);
+            this.dataGridView1.TabIndex = 40;
             // 
             // DeleteBookingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(837, 589);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnExitV);
             this.Controls.Add(this.btnSubmitV);
-            this.Controls.Add(this.txtRoomNum);
-            this.Controls.Add(this.lblRoomNum);
-            this.Controls.Add(this.txtCustID);
-            this.Controls.Add(this.lblCustID);
-            this.Controls.Add(this.dateTimePickerDepartureDate);
-            this.Controls.Add(this.lblLeaveDate);
-            this.Controls.Add(this.lblADate);
-            this.Controls.Add(this.dateTimePickerArrival);
             this.Controls.Add(this.lblHeadingEdit);
-            this.Controls.Add(this.listView1);
             this.Name = "DeleteBookingForm";
             this.Text = "DeleteBookingForm";
+            this.Load += new System.EventHandler(this.DeleteBookingForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,15 +115,7 @@
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnExitV;
         private System.Windows.Forms.Button btnSubmitV;
-        private System.Windows.Forms.TextBox txtRoomNum;
-        private System.Windows.Forms.Label lblRoomNum;
-        private System.Windows.Forms.TextBox txtCustID;
-        private System.Windows.Forms.Label lblCustID;
-        private System.Windows.Forms.DateTimePicker dateTimePickerDepartureDate;
-        private System.Windows.Forms.Label lblLeaveDate;
-        private System.Windows.Forms.Label lblADate;
-        private System.Windows.Forms.DateTimePicker dateTimePickerArrival;
         private System.Windows.Forms.Label lblHeadingEdit;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
