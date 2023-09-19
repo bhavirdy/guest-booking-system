62ddf75ee2a98f308da3415aba51ddbef0e9211f
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-09-19 15:12:35 +0200

    Added a Delete Customer Form

---
+++ GuestBookingSystem/Presentation/DeleteCustomerForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GuestBookingSystem.Presentation
+{
+    partial class DeleteCustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeadingEditC = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHeadingEditC
+            // 
+            this.lblHeadingEditC.AutoSize = true;
+            this.lblHeadingEditC.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.lblHeadingEditC.Font = new System.Drawing.Font("Arial Rounded MT Bold", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeadingEditC.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblHeadingEditC.Location = new System.Drawing.Point(12, 9);
+            this.lblHeadingEditC.Name = "lblHeadingEditC";
+            this.lblHeadingEditC.Size = new System.Drawing.Size(448, 54);
+            this.lblHeadingEditC.TabIndex = 4;
+            this.lblHeadingEditC.Text = "Delete a Customer";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(834, 323);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnCancel.Location = new System.Drawing.Point(32, 427);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(81, 31);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnClear.Location = new System.Drawing.Point(293, 427);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(85, 29);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSubmit.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnSubmit.Location = new System.Drawing.Point(649, 424);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(93, 32);
+            this.btnSubmit.TabIndex = 18;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // DeleteCustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblHeadingEditC);
+            this.Name = "DeleteCustomerForm";
+            this.Text = "DeleteCustomerForm";
+            this.Load += new System.EventHandler(this.DeleteCustomerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeadingEditC;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
---
+++ GuestBookingSystem/Presentation/DeleteCustomerForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuestBookingSystem.Presentation
+{
+    public partial class DeleteCustomerForm : Form
+    {
+        #region Data Members
+        DataSet ds = new DataSet();
+
+        //Don't know what the connection is
+        SqlConnection conDB = new SqlConnection();
+        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+        #endregion
+
+        #region Constructor
+
+        public DeleteCustomerForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Form Events
+
+        private void DeleteCustomerForm_Load(object sender, EventArgs e)
+        {
+
+            SqlCommand sqlSelect = new SqlCommand("SELECT * FROM Customer", conDB);
+
+            conDB.Open();
+            dataAdapter.SelectCommand = sqlSelect;
+            dataAdapter.Fill(ds, "Customer");
+            conDB.Close();
+
+
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "Customer";
+
+
+        }
+
+        #endregion 
+    }
+}
