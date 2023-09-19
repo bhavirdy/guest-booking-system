02f4f4acac27ac0b56342590d4ff08516bb3a5af
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-09-19 14:57:49 +0200

    Took Serviced out of Room

--- GuestBookingSystem/Business/Room.cs
+++ GuestBookingSystem/Business/Room.cs
@@ -11,22 +11,15 @@ namespace GuestBookingSystem.Business
 
         #region DataMembers
         private int roomID;
-        private bool serviced;
         private int numBeds;
-
+        private bool available;
         #endregion
 
         #region Property Members
         public int RoomID
         {
-            get { return roomID; } 
-            set {  roomID = value; }
-        }
-
-        public bool Serviced
-        {
-            get { return serviced; }
-            set { serviced = value; }
+            get { return roomID; }
+            set { roomID = value; }
         }
 
         public int NumBeds
@@ -35,29 +28,14 @@ namespace GuestBookingSystem.Business
             set { numBeds = value; }
         }
 
-        public int PricePerNight
-        {
-            get { return pricePerNight; }
-            set { pricePerNight = value; }
-        }
-
-        public bool Available
-        {
-            get { return available; }
-            set { value = available}
-        }
-
         #endregion
 
         #region Constructor
 
-        public Room(int roomIDTemp,  bool servicedTemp, int numBedsTemp, int priceTemp, bool availTemp)
-        {
+        public Room(int roomIDTemp, int numBedsTemp) { 
             roomID = roomIDTemp;
-            serviced = servicedTemp;
+     
             numBeds = numBedsTemp;
-            pricePerNight = priceTemp;
-            available = availTemp;
         }
 
         #endregion 
