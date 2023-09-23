ebdf9369bc1d239141c39124fc3e86fbe3c66599
Bhavishyan Reddy <bhavirdy@gmail.com>
2023-09-23 14:13:08 +0200

    shifted buttons

--- GuestBookingSystem/Presentation/CreateCustomerForm.Designer.cs
+++ GuestBookingSystem/Presentation/CreateCustomerForm.Designer.cs
@@ -141,7 +141,7 @@
             // 
             this.btnClearC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClearC.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.btnClearC.Location = new System.Drawing.Point(203, 415);
+            this.btnClearC.Location = new System.Drawing.Point(219, 415);
             this.btnClearC.Margin = new System.Windows.Forms.Padding(2);
             this.btnClearC.Name = "btnClearC";
             this.btnClearC.Size = new System.Drawing.Size(62, 24);
