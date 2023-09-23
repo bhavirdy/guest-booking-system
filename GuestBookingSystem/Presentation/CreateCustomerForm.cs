2c7cbae572e954e586d3c960485e8e3b41539e9a
Bhavishyan Reddy <bhavirdy@gmail.com>
2023-09-23 14:44:40 +0200

    submit validation works

--- GuestBookingSystem/Presentation/CreateCustomerForm.cs
+++ GuestBookingSystem/Presentation/CreateCustomerForm.cs
@@ -45,13 +45,8 @@ namespace GuestBookingSystem.Presentation
         //{
         //    InitializeComponent();
 
-        //    this.Load += CreateCustomerForm_Load1;
-        //    this.Activated += CreateCustomerForm_Activated;
         //    customerController = controllerTemp;
         //    isOpen = true;
-
-        //    customerController = controllerTemp;
-
         //}
 
         #endregion
@@ -59,13 +54,8 @@ namespace GuestBookingSystem.Presentation
 
         private void CreateCustomerForm_Activated(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
-        }
 
-        //private void CreateCustomerForm_Load1(object sender, EventArgs e)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        }
 
         #region Property Methods
 
@@ -75,10 +65,10 @@ namespace GuestBookingSystem.Presentation
             get { return isOpen; }
         }
 
-        #endregion 
+        #endregion
 
         #region Utility methods
-    
+
         private void ClearAll()
         {
 
@@ -95,14 +85,14 @@ namespace GuestBookingSystem.Presentation
 
         private void PopulateObject()
         {
-            //customer.StreetAddress = txtStreetA.Text;
-            //customer.Email = txtEmail.Text;
-            //customer.Name = txtName.Text;
-            //customer.PostalCode = txtPostalCode.Text;
-            //customer.Phone = txtPhoneNum.Text;
-            //customer.Surname = txtSurname.Text;
-            //customer.Suburb = txtSuburb.Text;
-            //customer.Country = txtCountry.Text;
+            customer.StreetAddress = txtStreetA.Text;
+            customer.Email = txtEmail.Text;
+            customer.Name = txtName.Text;
+            customer.PostalCode = txtPostalCode.Text;
+            customer.Phone = txtPhoneNum.Text;
+            customer.Surname = txtSurname.Text;
+            customer.TownOrCity = txtTownOrCity.Text;
+            customer.Province = txtProvince.Text;
         }
 
         #endregion
@@ -111,54 +101,47 @@ namespace GuestBookingSystem.Presentation
         private void btnSubmitC_Click(object sender, EventArgs e)
         {
 
-            //if (txtCity.Text.Equals("") || txtCountry.Text.Equals("") || txtEmail.Text.Equals("") || txtName.Text.Equals("") || txtPhoneNum.Text.Equals("") || txtPostalCode.Text.Equals("") || txtStreetA.Text.Equals("") || txtSuburb.Text.Equals("") || txtSurname.Text.Equals(""))
-            //{
-            //    MessageBox.Show("Please fill all the fields");
+            if (txtTownOrCity.Text.Equals("") || txtProvince.Text.Equals("") || txtEmail.Text.Equals("") || txtName.Text.Equals("") || txtPhoneNum.Text.Equals("") || txtPostalCode.Text.Equals("") || txtStreetA.Text.Equals("") || txtSurname.Text.Equals(""))
+            {
+                MessageBox.Show("Please fill all the fields");
 
-            //}
-            //else
-            //    if (txtPostalCode.TextLength != 4)
-
-            //if(txtCardNumber.Text.Equals("") || txtCity.Text.Equals("") || txtCountry.Text.Equals("") || txtEmail.Text.Equals("") || txtName.Text.Equals("") || txtPhoneNum.Text.Equals("") || txtPostalCode.Text.Equals("") || txtStreetA.Text.Equals("") || txtSuburb.Text.Equals("") || txtSurname.Text.Equals(""))
-            //{
-            //    MessageBox.Show("Please fill all the fields");
-
-            //}else
-            //    if(txtPostalCode.TextLength != 4)
-
-            //{
-            //    MessageBox.Show("Please enter a valid postal code");
-            //}
-            //else
-            //{
-            //    PopulateObject();
-
-            //    SqlCommand commIns = new SqlCommand("INSERT INTO Customer(CustomerID, Name, Surname, Email, StreetAddress, Town, PostalCode, Phone" +
-            //                                           "VALUES(@CustomerID, @Name, @Surname, @Email, @StreetAddress, @Town, @PostalCode, @Phone", conDB);
-
-            //    commIns.Parameters.Add("@CustomerID", SqlDbType.NVarChar, 50, "CustomerID").Value = txtCustID.Text;
-            //    commIns.Parameters.Add("@Name", SqlDbType.NVarChar, 50, "Name").Value = txtName.Text;
-            //    commIns.Parameters.Add("@Surname", SqlDbType.NVarChar, 50, "Surname").Value = txtSurname.Text;
-            //    commIns.Parameters.Add("@Email", SqlDbType.NVarChar, 50, "Email").Value = txtEmail.Text;
-            //    commIns.Parameters.Add("@StreetAddress", SqlDbType.NVarChar, 50, "StreetAddress").Value = txtStreetA.Text;
-            //    //commIns.Parameters.Add("@Suburb", SqlDbType.NVarChar, 50, "Suburb").Value = txtSuburb.Text;
-            //    commIns.Parameters.Add("@Town", SqlDbType.NVarChar, 50, "Town").Value = txtCity.Text;
-            //    commIns.Parameters.Add("@Country", SqlDbType.NVarChar, 50, "Country").Value = txtCountry.Text;
-            //    commIns.Parameters.Add("@PostalCode", SqlDbType.NVarChar, 50, "PostalCode").Value = txtPostalCode.Text;
-            //    commIns.Parameters.Add("@Phone", SqlDbType.NVarChar, 50, "Phone").Value = txtPhoneNum.Text;
-
-            //    commIns.ExecuteNonQuery();
-
-
-
-            //    MessageBox.Show("Customer entered!");
-            //    customerController.DataMaintanence(customer);
-            //    customerController.FinalizeChanges(customer);
-            //    ClearAll();
-
-
-                //ShowAll(true);
             }
+            else if (txtPostalCode.TextLength != 4) 
+            { 
+                MessageBox.Show("Please enter a valid postal code"); 
+            }
+        
+    
+        //else
+        //{
+        //    PopulateObject();
+
+        //    SqlCommand commIns = new SqlCommand("INSERT INTO Customer(CustomerID, Name, Surname, Email, StreetAddress, Town, PostalCode, Phone" +
+        //                                           "VALUES(@CustomerID, @Name, @Surname, @Email, @StreetAddress, @Town, @PostalCode, @Phone", conDB);
+
+        //    commIns.Parameters.Add("@CustomerID", SqlDbType.NVarChar, 50, "CustomerID").Value = txtCustID.Text;
+        //    commIns.Parameters.Add("@Name", SqlDbType.NVarChar, 50, "Name").Value = txtName.Text;
+        //    commIns.Parameters.Add("@Surname", SqlDbType.NVarChar, 50, "Surname").Value = txtSurname.Text;
+        //    commIns.Parameters.Add("@Email", SqlDbType.NVarChar, 50, "Email").Value = txtEmail.Text;
+        //    commIns.Parameters.Add("@StreetAddress", SqlDbType.NVarChar, 50, "StreetAddress").Value = txtStreetA.Text;
+        //    //commIns.Parameters.Add("@Suburb", SqlDbType.NVarChar, 50, "Suburb").Value = txtSuburb.Text;
+        //    commIns.Parameters.Add("@Town", SqlDbType.NVarChar, 50, "Town").Value = txtCity.Text;
+        //    commIns.Parameters.Add("@Country", SqlDbType.NVarChar, 50, "Country").Value = txtCountry.Text;
+        //    commIns.Parameters.Add("@PostalCode", SqlDbType.NVarChar, 50, "PostalCode").Value = txtPostalCode.Text;
+        //    commIns.Parameters.Add("@Phone", SqlDbType.NVarChar, 50, "Phone").Value = txtPhoneNum.Text;
+
+        //    commIns.ExecuteNonQuery();
+
+
+
+        //    MessageBox.Show("Customer entered!");
+        //    customerController.DataMaintanence(customer);
+        //    customerController.FinalizeChanges(customer);
+        //    ClearAll();
+
+
+        //ShowAll(true);
+        }
 
         private void CreateCustomerForm_FormClosed(object sender, FormClosedEventArgs e)
         {
@@ -181,37 +164,6 @@ namespace GuestBookingSystem.Presentation
             this.Close();
         }
     }
+    #endregion
 
-        //private void CreateCustomerForm_Load(object sender, EventArgs e)
-        //{
-
-
-
-        //    ShowAll(true);
-
-
-
-        //}
-
-        //private void lblHeadingCreateCustomer_Click(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void textBox1_TextChanged(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void lblEmail_Click(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //private void textBox1_TextChanged_1(object sender, EventArgs e)
-        //{
-
-        //}
-        #endregion 
-    
 }
