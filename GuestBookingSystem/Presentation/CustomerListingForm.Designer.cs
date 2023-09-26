d64d1ce9945bd7b4fd2c2387bb2497c44d0acecf
Bhavishyan Reddy <bhavirdy@gmail.com>
2023-09-26 14:17:11 +0200

    edited list view size

--- GuestBookingSystem/Presentation/CustomerListingForm.Designer.cs
+++ GuestBookingSystem/Presentation/CustomerListingForm.Designer.cs
@@ -58,7 +58,7 @@
             this.customerListView.HideSelection = false;
             this.customerListView.Location = new System.Drawing.Point(26, 12);
             this.customerListView.Name = "customerListView";
-            this.customerListView.Size = new System.Drawing.Size(744, 243);
+            this.customerListView.Size = new System.Drawing.Size(997, 243);
             this.customerListView.TabIndex = 2;
             this.customerListView.UseCompatibleStateImageBehavior = false;
             this.customerListView.SelectedIndexChanged += new System.EventHandler(this.customerListView_SelectedIndexChanged);
@@ -67,7 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1141, 450);
             this.Controls.Add(this.customerListView);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.btnDelete);
