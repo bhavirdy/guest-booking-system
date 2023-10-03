5fcb583485b94eeaea9adc8e6fb64c37119cdb4a
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-10-03 12:53:53 +0200

    Added confirmation letter form

---
+++ GuestBookingSystem/Presentation/ConfirmationLetter.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace GuestBookingSystem.Presentation
+{
+    partial class ConfirmationLetter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTotalOut = new System.Windows.Forms.Label();
+            this.lblDepartureDateOut = new System.Windows.Forms.Label();
+            this.lblArrivalDateOut = new System.Windows.Forms.Label();
+            this.lblCustNameOut = new System.Windows.Forms.Label();
+            this.lblRefNumOut = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTotalOut
+            // 
+            this.lblTotalOut.AutoSize = true;
+            this.lblTotalOut.Location = new System.Drawing.Point(196, 277);
+            this.lblTotalOut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotalOut.Name = "lblTotalOut";
+            this.lblTotalOut.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalOut.TabIndex = 50;
+            // 
+            // lblDepartureDateOut
+            // 
+            this.lblDepartureDateOut.AutoSize = true;
+            this.lblDepartureDateOut.Location = new System.Drawing.Point(193, 231);
+            this.lblDepartureDateOut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDepartureDateOut.Name = "lblDepartureDateOut";
+            this.lblDepartureDateOut.Size = new System.Drawing.Size(35, 13);
+            this.lblDepartureDateOut.TabIndex = 49;
+            this.lblDepartureDateOut.Text = "label7";
+            // 
+            // lblArrivalDateOut
+            // 
+            this.lblArrivalDateOut.AutoSize = true;
+            this.lblArrivalDateOut.Location = new System.Drawing.Point(193, 190);
+            this.lblArrivalDateOut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblArrivalDateOut.Name = "lblArrivalDateOut";
+            this.lblArrivalDateOut.Size = new System.Drawing.Size(35, 13);
+            this.lblArrivalDateOut.TabIndex = 48;
+            this.lblArrivalDateOut.Text = "label7";
+            // 
+            // lblCustNameOut
+            // 
+            this.lblCustNameOut.AutoSize = true;
+            this.lblCustNameOut.Location = new System.Drawing.Point(191, 148);
+            this.lblCustNameOut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCustNameOut.Name = "lblCustNameOut";
+            this.lblCustNameOut.Size = new System.Drawing.Size(35, 13);
+            this.lblCustNameOut.TabIndex = 47;
+            this.lblCustNameOut.Text = "label7";
+            // 
+            // lblRefNumOut
+            // 
+            this.lblRefNumOut.AutoSize = true;
+            this.lblRefNumOut.Location = new System.Drawing.Point(191, 102);
+            this.lblRefNumOut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRefNumOut.Name = "lblRefNumOut";
+            this.lblRefNumOut.Size = new System.Drawing.Size(25, 13);
+            this.lblRefNumOut.TabIndex = 46;
+            this.lblRefNumOut.Text = "Null";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(356, 367);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 26);
+            this.button1.TabIndex = 45;
+            this.button1.Text = "Proceed";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label6.Location = new System.Drawing.Point(43, 278);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 44;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label5.Location = new System.Drawing.Point(41, 232);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 17);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "Departure Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(41, 190);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Arrival Date: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(41, 148);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 17);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Customer Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(41, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 17);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Reference Number: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(27, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 44);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Confirmation Letter";
+            // 
+            // ConfirmationLetter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotalOut);
+            this.Controls.Add(this.lblDepartureDateOut);
+            this.Controls.Add(this.lblArrivalDateOut);
+            this.Controls.Add(this.lblCustNameOut);
+            this.Controls.Add(this.lblRefNumOut);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ConfirmationLetter";
+            this.Text = "ConfirmationLetter";
+            this.Load += new System.EventHandler(this.ConfirmationLetter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTotalOut;
+        private System.Windows.Forms.Label lblDepartureDateOut;
+        private System.Windows.Forms.Label lblArrivalDateOut;
+        private System.Windows.Forms.Label lblCustNameOut;
+        private System.Windows.Forms.Label lblRefNumOut;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
