a2c304d4a9e2558fb708a5554d3170339258c57b
Caitlin Poole <PLXCAI002@myuct.ac.za>
2023-10-03 13:52:17 +0200

    Added components to the Booking Listing Form

--- GuestBookingSystem/Presentation/MDIParent.Designer.cs
+++ GuestBookingSystem/Presentation/MDIParent.Designer.cs
@@ -33,10 +33,13 @@
             this.viewTablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bookingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.checkAvailabilityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.customersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
-            this.customerListingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bookingListingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerListingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.occupancyReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.locationReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +49,8 @@
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.viewTablesToolStripMenuItem,
             this.bookingsToolStripMenuItem,
-            this.customersToolStripMenuItem});
+            this.customersToolStripMenuItem,
+            this.reportsToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
             this.menuStrip.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
@@ -73,9 +77,15 @@
             // checkAvailabilityToolStripMenuItem
             // 
             this.checkAvailabilityToolStripMenuItem.Name = "checkAvailabilityToolStripMenuItem";
-            this.checkAvailabilityToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkAvailabilityToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.checkAvailabilityToolStripMenuItem.Text = "Check Availability";
             this.checkAvailabilityToolStripMenuItem.Click += new System.EventHandler(this.checkAvailabilityToolStripMenuItem_Click);
+            // 
+            // bookingListingToolStripMenuItem
+            // 
+            this.bookingListingToolStripMenuItem.Name = "bookingListingToolStripMenuItem";
+            this.bookingListingToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.bookingListingToolStripMenuItem.Text = "Booking Listing";
             // 
             // customersToolStripMenuItem
             // 
@@ -88,15 +98,30 @@
             // customerListingToolStripMenuItem
             // 
             this.customerListingToolStripMenuItem.Name = "customerListingToolStripMenuItem";
-            this.customerListingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.customerListingToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
             this.customerListingToolStripMenuItem.Text = "Customer Listing";
             this.customerListingToolStripMenuItem.Click += new System.EventHandler(this.customerListingToolStripMenuItem_Click);
             // 
-            // bookingListingToolStripMenuItem
+            // reportsToolStripMenuItem
             // 
-            this.bookingListingToolStripMenuItem.Name = "bookingListingToolStripMenuItem";
-            this.bookingListingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.bookingListingToolStripMenuItem.Text = "Booking Listing";
+            this.reportsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.occupancyReportToolStripMenuItem,
+            this.locationReportToolStripMenuItem});
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // occupancyReportToolStripMenuItem
+            // 
+            this.occupancyReportToolStripMenuItem.Name = "occupancyReportToolStripMenuItem";
+            this.occupancyReportToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.occupancyReportToolStripMenuItem.Text = "Occupancy Report";
+            // 
+            // locationReportToolStripMenuItem
+            // 
+            this.locationReportToolStripMenuItem.Name = "locationReportToolStripMenuItem";
+            this.locationReportToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.locationReportToolStripMenuItem.Text = "Location Report";
             // 
             // MDIParent
             // 
@@ -126,6 +151,9 @@
         private System.Windows.Forms.ToolStripMenuItem viewTablesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bookingListingToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem customerListingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem occupancyReportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem locationReportToolStripMenuItem;
     }
 }
 
